4d8477241664e8b57868ba948770ec3e0d099e2a
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2020-05-16 20:31:27 -0400

    POC Done

---
+++ data/poc.py
@@ -0,0 +1,16 @@
+from pyspark import SparkContext, SparkConf
+
+conf = SparkConf().setAppName("Spark Count")
+sc = SparkContext(conf=conf)
+
+# the first step involves reading the source text file from HDFS 
+text_file = sc.textFile("/tmp/data/poc.py")
+
+# this step involves the actual computation for reading the number of words in the file
+# flatmap, map and reduceByKey are all spark RDD functions
+counts = text_file.flatMap(lambda line: line.split(" ")) \
+             .map(lambda word: (word, 1)) \
+             .reduceByKey(lambda a, b: a + b)
+
+# the final step is just saving the result.
+counts.saveAsTextFile("/tmp/data/resultado")
